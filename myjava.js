359fac442624d19132c3aeba040e931f2adf2287
Elisa <slatonea@gmail.com>
2019-12-04 13:01:57 -0500

    updated portfolio website

---
+++ myjava.js
@@ -0,0 +1,46 @@
+// Working Clock
+function showTime(){
+    var date = new Date();
+    var h = date.getHours(); // 0 - 23
+    var m = date.getMinutes(); // 0 - 59
+    var s = date.getSeconds(); // 0 - 59
+    var session = "AM";
+
+    if(h == 0){
+        h = 12;
+    }
+
+    if(h > 12){
+        h = h - 12;
+        session = "PM";
+    }
+
+    h = (h < 10) ? "0" + h : h;
+    m = (m < 10) ? "0" + m : m;
+    s = (s < 10) ? "0" + s : s;
+
+    var time = h + ":" + m + ":" + s + " " + session;
+    document.getElementById("displayclock").innerText = time;
+    document.getElementById("displayclock").textContent = time;
+
+    setTimeout(showTime, 1000);
+
+}
+
+// Date Real-time
+showTime();
+
+n =  new Date();
+y = n.getFullYear();
+m = n.getMonth() + 1;
+d = n.getDate();
+document.getElementById("date").innerHTML = m + "/" + d + "/" + y;
+
+var degrees = 0;
+$( ".circle" ).click(function() {
+  degrees+=90;
+    $(this).css("transform","rotate("+degrees+"deg)");
+});
+
+
+// Animation
